a5a56fcbadf9b29fb6ff9c1a31ef6e2e04384ada
Alex <wonderfuljinx@gmail.com>
2022-12-06 01:23:50 +0300

    asd

---
+++ TrialP.Products/Services/Abstract/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace TrialP.Products.Services.Abstract
+{
+    public interface IEmailService
+    {
+        public void SendEmail(string email, string body);
+        public Task SendEmailAsync(string email, string body);
+    }
+}
---
+++ TrialP.Products/Services/Domain/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using MimeKit;
+using System.Net;
+using System.Net.Mail;
+using TrialP.Products.Services.Abstract;
+
+namespace TrialP.Products.Services.Domain
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string email, string body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SendEmailAsync(string email, string body)
+        {
+            await Send(email, body);
+        }
+
+        private async Task Send(string email, string body)
+        {
+            MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
+            MailAddress to = new MailAddress(email);
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Заказ товаров";
+            m.Body = body;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("somemail@gmail.com", "mypassword");
+            smtp.EnableSsl = true;
+            await smtp.SendMailAsync(m);
+        }
+    }
+}
